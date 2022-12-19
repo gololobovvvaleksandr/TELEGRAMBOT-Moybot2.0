4a018f447dede29718487d7ab9d14a8da9914b0f
gololobovvvaleksandr <114258402+gololobovvvaleksandr@users.noreply.github.com>
2022-12-19 16:47:55 +0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+TOKEN = '5929580850:AAGW7jcGoT5pDL6RpVcxBOtnIoVcOQ6_pnk'
+CURRENCY_API = 'd8ef98411b596e7315d490350c9559b01433c8259bc2cb7cda17702997f4f11b'
+#AVALIABLE CURRENCY
+keys = {
+    'евро' : 'EUR',
+    'рубль' : 'RUB',
+    'доллар' : 'USD'
+}
